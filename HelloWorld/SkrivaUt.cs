c774e3a1c87ca29a0a6c320620190fbaf916bec5
svennekingen <claeseriktobias@hotmail.com>
2017-01-19 13:18:27 +0100

    Initial commit
    .

---
+++ HelloWorld/SkrivaUt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class SkrivaUt
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
